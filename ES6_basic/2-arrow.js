8d6c337a98ab60b2c688764a243bd20b09bd9132
GRohithSai <rohithsai@brightcomgroup.com>
2023-03-02 04:35:57 +0000

    M

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighborhoodsList() {
-  this.sanFransicoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    self.sanFransiscoNeighborhoods.push(newNeighborhood);
-    return self.sanFransiscoNeightborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
