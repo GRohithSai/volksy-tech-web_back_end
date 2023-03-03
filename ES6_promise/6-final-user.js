08c8d983c4c6276c861afd32ea19ad37921e9659
GRohithSai <rohithsai@brightcomgroup.com>
2023-03-03 06:29:39 +0000

    M

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName)  const userData = await signUpUser(firstName, lastName).then((data) => ({
+export default async function handleProfileSignup(firstName, lastName, fileName)
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
     status: 'fulfilled',
     value: data,
   }));
