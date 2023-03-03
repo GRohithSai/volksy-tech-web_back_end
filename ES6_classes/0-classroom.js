419a0347a8da0ab6208d54be436b8d4343a03603
GRohithSai <rohithsai@brightcomgroup.com>
2023-03-03 11:34:22 +0000

    M

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,19 +1,5 @@
-export default function getListStudents() {
-  return [
-    {
-      firstName: 'Guillaume',
-      id: 1,
-      location: 'San Francisco',
-    },
-    {
-      firstName: 'James',
-      id: 2,
-      location: 'Columbia',
-    },
-    {
-      firstName: 'Serena',
-      id: 5,
-      location: 'San Francisco',
-    },
-  ];
+export default class ClassRoom {
+  constructor(maxStudentSize) {
+    this._maxStudentsSize = maxStudentSize;
+  }
 }
