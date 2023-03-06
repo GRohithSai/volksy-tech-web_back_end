5e287249fc1586287bdee8c06fa933bd13cc3274
GRohithSai <rohithsai@brightcomgroup.com>
2023-03-06 12:22:16 +0000

    M

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,7 @@
 export default function groceriesList() {
   const groceries = new Map();
   groceries.set('Apples', 10);
-  grocreies.set('Tomatoes', 10);
+  groceries.set('Tomatoes', 10);
   groceries.set('pasta', 1);
   groceries.set('Rice', 1);
   groceries.set('Banana', 5);
