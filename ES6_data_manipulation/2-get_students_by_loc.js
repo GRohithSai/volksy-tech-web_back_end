45250abb70cb0f6a84b4cbffc5f1ec449d90653d
GRohithSai <rohithsai@brightcomgroup.com>
2023-03-06 11:01:59 +0000

    M

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(students, city) {
   // check arg is an array before using filter
-  if (object.getPrototypeOf(students) === Array.prototype) {
+  if (Object.getPrototypeOf(students) === Array.prototype) {
     return students.filter((items) => items.location === city);
   }
   return [];
